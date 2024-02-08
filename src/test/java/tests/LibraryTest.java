dd8c8c25912f58d9ac6b2aa95dec7cfb682ab094
KareemZafan <kareemm.zafan@gmail.com>
2024-02-08 12:34:33 +0200

    Push failed test

--- src/test/java/tests/LibraryTest.java
+++ src/test/java/tests/LibraryTest.java
@@ -16,7 +16,7 @@ public class LibraryTest {
         Library library= new Library();
         Book myBook = new Book("Tdd By Examples","Kent Beck",  "13$");
         library.addBooks(List.of(myBook, new Book("C Programming", "Denis Ritchie", "20$")));
-        assertTrue(library.getBooksCount() == 2 );
+        assertTrue(library.getBooksCount() == 0 );
         assertTrue(library.getCurrentBooks().contains(myBook));
     }
 
